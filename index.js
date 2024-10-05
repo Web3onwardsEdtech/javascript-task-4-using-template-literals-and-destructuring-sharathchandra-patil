67446a9f03f11b8a7fb5620f63fc766dddd63b5a
Sharathchandra <sharathchandra9204@gmail.com>
2024-10-05 15:55:52 +0530

    Task 4 submission

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const userName = process.argv[2];
+const userAge = parseInt(process.argv[3]);
+const userCountry = process.argv[4];
 
+function createMessage(name, age, country) {
+  let message=`Hello,my name is ${name},i am ${age} years old from ${country}`;
+  console.log(message);
+}
+createMessage(userName, userAge, userCountry);
